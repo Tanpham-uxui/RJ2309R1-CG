5ea7479a34c06654612a9c150c9f07efa42ebcb3
Tanpham-uxui <tanpham.uxui@gmail.com>
2023-10-25 16:33:30 +0700

    use-state homework

--- Day07-Hooks/hooks/src/App.js
+++ Day07-Hooks/hooks/src/App.js
@@ -6,6 +6,7 @@ import RadioGroup from "./Component/RadioGroup";
 import CheckboxGroup from "./Component/CheckboxGroup";
 import ConditionalRendering from "./Component/HomeWork/ExpandCollapse";
 import Calculator from "./Component/HomeWork/Calculator";
+import Home from "./Component/HomeWork/LoginForm/LoginWithBootStrap";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         {/*<AppCount/>*/}
         {/*<CheckboxGroup/>*/}
         {/*<ConditionalRendering/>*/}
-        <Calculator/>
+        {/*<Calculator/>*/}
+        <Home/>
       </div>
   );
 }
