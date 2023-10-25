3136512e7024787a9aadb8f6128945fb26fdf72d
Tanpham-uxui <tanpham.uxui@gmail.com>
2023-10-26 02:13:42 +0700

    use-effect

--- Day07-Hooks/hooks/src/App.js
+++ Day07-Hooks/hooks/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import UseEffect from "./UseEffect/UseEffect";
 
 function App() {
+  const [toggle, setToggle] = useState(false)
   return (
-    <div className="container">
-
-    </div>
+      <div className="container mt-2">
+        <button className='mb-2' onClick={() => setToggle(!toggle)}>Toggle Component</button>
+        {toggle && <UseEffect/>}
+      </div>
   );
 }
 
--- Day07-Hooks/hooks/src/UseEffect/UseEffect.jsx
+++ Day07-Hooks/hooks/src/UseEffect/UseEffect.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function UseEffect(){
-  console.log('component mounted')
+  const [content, setContent] = useState()
+  useEffect(()=>{
+    console.log('test')
+  })
+  return(
+      <>
+        <input type='text' className='form-control mb-2'
+               onInput={(e)=>setContent(e.target.value)}/>
+        <h1>{content}</h1>
+      </>
+  )
 }
+export default UseEffect
