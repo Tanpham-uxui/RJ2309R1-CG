2abb1035fe3744c5c95300013acc99b6593f095e
Tanpham-uxui <tanpham.uxui@gmail.com>
2023-11-27 00:18:55 +0700

    api client update

--- Day07-Hooks/hooks/src/index.js
+++ Day07-Hooks/hooks/src/index.js
@@ -7,20 +7,26 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import {BrowserRouter} from "react-router-dom";
 import {createTheme, ThemeProvider} from "@mui/material";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const darkTheme = createTheme({
-  palette:{
-    mode:'dark',
+  palette: {
+    mode: 'dark',
   },
 })
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-      </ThemeProvider>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={darkTheme}>
+        </ThemeProvider>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+          <App/>
+        </DevSupport>
+      </BrowserRouter>
+    </React.StrictMode>
 );
 
